382615e57ade6a2b5587d0fccd87ecb3f6377548
Jeremiah Brannon <email@jeremiahbrannon.com>
2026-09-20 00:55:24 -0700

    update waybar workspaces

--- .config/hypr/hyprland/monitors.lua
+++ .config/hypr/hyprland/monitors.lua
@@ -12,3 +12,13 @@ hl.monitor({
   position = "2560x0",
   scale    = "1",
 })
+
+-- Pin workspaces 1-5 to DP-1 and 6-10 to HDMI-A-1 so mainMod+[0-9] and
+-- waybar's per-monitor workspace buttons always agree on which screen a
+-- workspace lives on. See https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+for i = 1, 5 do
+  hl.workspace_rule({ workspace = tostring(i), monitor = "DP-1", default = (i == 1), persistent = true })
+end
+for i = 6, 10 do
+  hl.workspace_rule({ workspace = tostring(i), monitor = "HDMI-A-1", default = (i == 6), persistent = true })
+end
